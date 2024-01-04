47520b5110f6b1c4e9947486a6f25af05984c970
labcst <155661161+labcst@users.noreply.github.com>
2024-01-04 22:33:25 +0530

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    private Socket socket = null;
+    private BufferedReader input = null;
+    private DataOutputStream out = null;
+
+    public Client(String address, int port) {
+        try {
+            socket = new Socket(address, port);
+            System.out.println("Connected");
+
+            // Use BufferedReader to read lines of text
+            input = new BufferedReader(new InputStreamReader(System.in));
+            out = new DataOutputStream(socket.getOutputStream());
+        } catch (UnknownHostException u) {
+            System.out.println(u);
+            return;
+        } catch (IOException i) {
+            System.out.println(i);
+            return;
+        }
+
+        String line = "";
+        while (!line.equals("Over")) {
+            try {
+                // Use readLine() from BufferedReader
+                line = input.readLine();
+                out.writeUTF(line);
+            } catch (IOException i) {
+                System.out.println(i);
+            }
+        }
+
+        try {
+            input.close();
+            out.close();
+            socket.close();
+        } catch (IOException i) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String args[]) {
+        Client client = new Client("127.0.0.1", 5000);
+    }
+}
+
